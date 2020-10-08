e6495fc3c355efa14bcfa5f7f7f99c1eb38f4c9c
Paul Bredenberg <paulbredenberg@gmail.com>
2020-10-08 09:07:35 -0400

    chore: initial commit

---
+++ src/.vuepress/components/demo-component.vue
@@ -0,0 +1,15 @@
+<template>
+  <p class="demo">
+    {{ msg }}
+  </p>
+</template>
+
+<script lang="ts">
+  export default {
+    data() {
+      return {
+        msg: 'Hello this is <demo-component>'
+      }
+    }
+  }
+</script>
